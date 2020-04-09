30fcd9e598ff1db1458262173e03b45530423773
Ross Ferreira <ross.p.ferreira@gmail.com>
2020-04-09 08:21:25 +0100

    Updates

---
+++ precache-manifest.99ac6d749fda2a96a76caae56b39452d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bc469a17715c927ebee77de448bccda",
+    "url": "/hazard_perception_app/index.html"
+  },
+  {
+    "revision": "0ec2b34ced476b7c3f06",
+    "url": "/hazard_perception_app/static/js/2.b312e4be.chunk.js"
+  },
+  {
+    "revision": "aef280276b8d6f56e9d8dc46988119d9",
+    "url": "/hazard_perception_app/static/js/2.b312e4be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7561ced6f18ad90443b2",
+    "url": "/hazard_perception_app/static/js/main.03ca7cc6.chunk.js"
+  },
+  {
+    "revision": "418dca5377622671c720",
+    "url": "/hazard_perception_app/static/js/runtime-main.2c0528aa.js"
+  }
+]);
